11e5fb7171d49abba0aad99eee0e119b9ee7bab3
Alonso Isauro <aisauro@ucsc.edu>
2026-01-22 00:45:58 -0800

    only installing ros2 with docker container

--- docker/run.sh
+++ docker/run.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
-# run.sh - Start ROS2 + Webots container (GPU + WSLg / Linux)
+# run.sh - Start ROS2 container (plug-and-play with native Webots on WSL2)
 
 # Stop any old container
 docker rm -f rover_dev 2>/dev/null || true
 
-# Detect OS and set DISPLAY
-OS_NAME=$(uname -r)
-if [[ "$OS_NAME" == *"microsoft"* ]]; then
-    # Inside WSL
-    HOST_DISPLAY=$DISPLAY
-else
-    # Linux / Mac
-    HOST_DISPLAY=$DISPLAY
-fi
+# Start container in detached mode
+docker compose up -d
 
-# Start container with GPU support
-docker run -it \
-  -e DISPLAY=$DISPLAY \
-  -v /mnt/wslg/.X11-unix:/tmp/.X11-unix \
-  -v $(pwd)/workspace:/workspace \
-  --name rover_dev \
-  docker-rover \
-  bash
+# Attach to container bash
+docker exec -it rover_dev bash
